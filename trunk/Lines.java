9f26766d6b835b5f22ab3d9f21c710dd68e8e48b
ignacio.cerda68@gmail.com <ignacio.cerda68@gmail.com@07ff8523-55fe-4946-0912-2cfe72ba2567>
2014-10-30 19:00:44 +0000

    se agregaron las clases del proyecto por primera vez

---
+++ trunk/Lines.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lines here.
+ * 
+ * @author (Cerda Varela Ignacio) 
+ * @version (a version number or a date)
+ */
+public class Lines extends Actor
+{
+    public Lines(int dir)
+    {
+        this.turn(dir);
+    }
+    /**
+     * Act - do whatever the Lines wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        
+    }    
+    
+    
+}
